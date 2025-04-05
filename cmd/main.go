ec4135caf7946e556deeca039f0b070b11cc4454
Panachai <panachai.ny@gmail.com>
2025-04-06 01:33:26 +0700

    init

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"template-golang/config"
+	"template-golang/database"
+	"template-golang/server"
+)
+
+func main() {
+	conf := config.GetConfig()
+	db := database.NewPostgresDatabase(conf)
+	server.NewEchoServer(conf, db).Start()
+}

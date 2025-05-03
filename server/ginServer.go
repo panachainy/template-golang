1239b691dd44ad61bfa99ac985163f80ab975e53
panachainy <panachai.ny@gmail.com>
2025-05-03 19:13:20 +0700

    Update ginServer.go

--- server/ginServer.go
+++ server/ginServer.go
@@ -10,7 +10,15 @@ import (
 	cockroachRepositories "template-golang/modules/cockroach/repositories"
 	cockroachUsecases "template-golang/modules/cockroach/usecases"
 
+	// docs "template-golang/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
 	"github.com/gin-gonic/gin"
+)
+
+const (
+	apiV1Path = "/api/v1"
 )
 
 type ginServer struct {
@@ -29,16 +37,28 @@ func NewGinServer(conf *config.Config, db database.Database) Server {
 }
 
 func (s *ginServer) Start() {
-	s.router.GET("/v1/healthz", func(c *gin.Context) {
+	v1 := s.router.Group(apiV1Path)
+
+	v1.GET("/healthz", func(c *gin.Context) {
 		c.String(http.StatusOK, "OK")
 	})
+
 	s.initializeCockroachHttpHandler()
 
 	serverUrl := fmt.Sprintf(":%d", s.conf.Server.Port)
 	s.router.Run(serverUrl)
 }
 
+func (s *ginServer) initSwagger() {
+	s.router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	ginSwagger.WrapHandler(swaggerfiles.Handler,
+		ginSwagger.URL("http://localhost:8080/swagger/doc.json"),
+		ginSwagger.DefaultModelsExpandDepth(-1))
+}
+
 func (s *ginServer) initializeCockroachHttpHandler() {
+
 	cockroachPostgresRepository := cockroachRepositories.NewCockroachPostgresRepository(s.db)
 	cockroachFCMMessaging := cockroachRepositories.NewCockroachFCMMessaging()
 	cockroachUsecase := cockroachUsecases.NewCockroachUsecaseImpl(
@@ -47,6 +67,7 @@ func (s *ginServer) initializeCockroachHttpHandler() {
 	)
 	cockroachHttpHandler := cockroachHandlers.NewCockroachHttpHandler(cockroachUsecase)
 
-	cRouters := s.router.Group("/v1/cockroach")
-	cRouters.POST("", cockroachHttpHandler.DetectCockroach)
+	v1 := s.router.Group(apiV1Path)
+	cockroachRouters := v1.Group("/cockroach")
+	cockroachRouters.POST("", cockroachHttpHandler.DetectCockroach)
 }

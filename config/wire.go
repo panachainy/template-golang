093e5ad0f54d1ce4f55f33b81918c5a7ce0a2349
panachainy <panachai.ny@gmail.com>
2025-05-04 22:01:05 +0700

    init wire & apply config

---
+++ config/wire.go
@@ -0,0 +1,16 @@
+//go:build wireinject
+// +build wireinject
+
+//go:generate wire
+package config
+
+import "github.com/google/wire"
+
+var ProviderSet = wire.NewSet(
+	GetConfig,
+)
+
+func Wire() (*Config, error) {
+	wire.Build(ProviderSet)
+	return &Config{}, nil
+}

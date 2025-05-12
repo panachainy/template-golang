d9d71683f89ae08683c6c274e9223543a1b47d0e
panachainy <panachai.ny@gmail.com>
2025-05-12 09:05:08 +0700

    refactor: update LINE provider configuration to use config values

--- modules/auth/usecases/authUsecaseImpl.go
+++ modules/auth/usecases/authUsecaseImpl.go
@@ -11,14 +11,12 @@ type authUsecaseImpl struct {
 }
 
 func Provide(conf *config.Config) *authUsecaseImpl {
-
 	goth.UseProviders(
-		// line.New(os.Getenv("LINE_KEY"), os.Getenv("LINE_SECRET"), "http://localhost:3000/auth/line/callback", "profile", "openid", "email"),
 		line.New(conf.Auth.Line.ClientID, conf.Auth.Line.ClientSecret, "http://localhost:3000/auth/line/callback", "profile", "openid", "email"),
 
+		// TODO: implement other providers
 		// facebook.New(os.Getenv("FACEBOOK_KEY"), os.Getenv("FACEBOOK_SECRET"), "http://localhost:3000/auth/facebook/callback"),
 		// google.New(os.Getenv("GOOGLE_KEY"), os.Getenv("GOOGLE_SECRET"), "http://localhost:3000/auth/google/callback"),
-
 		// instagram.New(os.Getenv("INSTAGRAM_KEY"), os.Getenv("INSTAGRAM_SECRET"), "http://localhost:3000/auth/instagram/callback"),
 		// apple.New(os.Getenv("APPLE_KEY"), os.Getenv("APPLE_SECRET"), "http://localhost:3000/auth/apple/callback", nil, apple.ScopeName, apple.ScopeEmail),
 	)

9947999ea75aa9ddae01334596222c48ff245434
Panachai <panachai.ny@gmail.com>
2025-07-08 23:42:24 +0700

    clean sqlite

--- database/wire.go
+++ database/wire.go
@@ -15,19 +15,7 @@ var PostgresProviderSet = wire.NewSet(
 	wire.Bind(new(Database), new(*postgresDatabase)),
 )
 
-// SQLiteProviderSet provides SQLite database implementation
-var SQLiteProviderSet = wire.NewSet(
-	ProvideSqliteDatabase,
-	wire.Bind(new(Database), new(*SqliteDatabase)),
-)
-
 func WirePostgres(conf *config.Config) (Database, error) {
 	wire.Build(PostgresProviderSet)
 	return &postgresDatabase{}, nil
-}
-
-// WireSQLite creates a SQLite database instance using dependency injection
-func WireSQLite(dsn string, logMode bool) (Database, error) {
-	wire.Build(SQLiteProviderSet)
-	return &SqliteDatabase{}, nil
 }
